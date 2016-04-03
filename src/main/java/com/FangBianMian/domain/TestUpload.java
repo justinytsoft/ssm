3a2370e870d1f1070b8c332802f5b25da35a123a
justi <justi@192.168.0.105>
2016-04-03 22:31:39 +0800

    fd

---
+++ src/main/java/com/FangBianMian/domain/TestUpload.java
@@ -0,0 +1,33 @@
+package com.FangBianMian.domain;
+
+public class TestUpload {
+
+	private Integer id;
+	private String name;
+	private String head;
+	private String doc;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHead() {
+		return head;
+	}
+	public void setHead(String head) {
+		this.head = head;
+	}
+	public String getDoc() {
+		return doc;
+	}
+	public void setDoc(String doc) {
+		this.doc = doc;
+	}
+}
