984fc2a00846450b1b69e31e2378a0699034d15e
justi <justi@192.168.0.105>
2016-04-04 23:03:40 +0800

    54654

--- src/main/java/com/FangBianMian/timer/DelTempFileTimer.java
+++ src/main/java/com/FangBianMian/timer/DelTempFileTimer.java
@@ -1,7 +1,13 @@
 package com.FangBianMian.timer;
 
-public class DelTempFileTimer {
-	public void execute(){
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class DelTempFileTimer implements Job{
+
+	@Override
+	public void execute(JobExecutionContext jec) throws JobExecutionException {
 		System.out.println("DelTempFileTimer");
 	}
 }
