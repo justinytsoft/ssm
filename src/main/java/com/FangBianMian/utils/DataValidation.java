f39694b84325fecb0486057834d39bb91e83197e
justinytsoft <justinytsoft@sina.com>
2016-04-13 12:54:04 +0800

    a

---
+++ src/main/java/com/FangBianMian/utils/DataValidation.java
@@ -0,0 +1,62 @@
+package com.FangBianMian.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 校验传入数据
+ *
+ */
+public class DataValidation {
+
+	/** 
+     * 检测邮箱地址是否合法 
+     * @param email 
+     * @return true合法 false不合法 
+     */  
+    public static boolean isEmail(String email){  
+    	if (null==email || "".equals(email)){
+    		return false;    
+    	}
+		Pattern p =  Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
+		Matcher m = p.matcher(email);  
+		return m.matches();  
+    }  
+    
+	/**
+	 * 手机号验证
+	 * 
+	 * @param  str
+	 * @return 验证通过返回true
+	 */
+	public static boolean isMobile(String str) { 
+		Pattern p = null;
+		Matcher m = null;
+		boolean b = false; 
+		p = Pattern.compile("^[1][3,4,5,8][0-9]{9}$"); // 验证手机号
+		m = p.matcher(str);
+		b = m.matches(); 
+		return b;
+	}
+	/**
+	 * 电话号码验证
+	 * 
+	 * @param  str
+	 * @return 验证通过返回true
+	 */
+	public static boolean isPhone(String str) { 
+		Pattern p1 = null,p2 = null;
+		Matcher m = null;
+		boolean b = false;  
+		p1 = Pattern.compile("^[0][1-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
+		p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
+		if(str.length() >9)
+		{	m = p1.matcher(str);
+ 		    b = m.matches();  
+		}else{
+			m = p2.matcher(str);
+ 			b = m.matches(); 
+		}  
+		return b;
+	}
+}
