db26ce0b7a79195845576e521c5fbafd6e25e015
justinytsoft <justinytsoft@sina.com>
2016-04-14 17:59:28 +0800

    玩得起

--- src/main/webapp/assets/plugins/easyui/custom/common.js
+++ src/main/webapp/assets/plugins/easyui/custom/common.js
@@ -40,6 +40,36 @@ function messagerCommon(){
 	$(".messager-icon").remove();
 }
 
+/*文本框清除功能*/
+$.extend($.fn.textbox.methods, {
+    addClearBtn: function(jq, iconCls){
+        return jq.each(function(){
+            var t = $(this);
+            var opts = t.textbox('options');
+            opts.icons = opts.icons || [];
+            opts.icons.unshift({
+                iconCls: iconCls,
+                handler: function(e){
+                    $(e.data.target).textbox('clear').textbox('textbox').focus();
+                    $(this).css('visibility','hidden');
+                }
+            });
+            t.textbox();
+            if (!t.textbox('getText')){
+                t.textbox('getIcon',0).css('visibility','hidden');
+            }
+            t.textbox('textbox').bind('keyup', function(){
+                var icon = t.textbox('getIcon',0);
+                if ($(this).val()){
+                    icon.css('visibility','visible');
+                } else {
+                    icon.css('visibility','hidden');
+                }
+            });
+        });
+    }
+});
+
 /*转换树节点*/
 function convert(rows){
 	//判断是不是根节点, 是, 返回true
