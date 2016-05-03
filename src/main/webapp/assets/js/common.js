7154ae0edbfb25df20b5fe374873e00f42fbe8ff
Administrator <Administrator@MS-20160426JFHL>
2016-05-03 11:56:59 +0800

    darwe

--- src/main/webapp/assets/js/common.js
+++ src/main/webapp/assets/js/common.js
@@ -1,3 +1,27 @@
+//日期格式化 new Date().format("yyyy年MM月dd日 hh:mm");
+Date.prototype.format = function(format){ 
+	var o = { 
+	"M+" : this.getMonth()+1, //month 
+	"d+" : this.getDate(), //day 
+	"h+" : this.getHours(), //hour 
+	"m+" : this.getMinutes(), //minute 
+	"s+" : this.getSeconds(), //second 
+	"q+" : Math.floor((this.getMonth()+3)/3), //quarter 
+	"S" : this.getMilliseconds() //millisecond 
+	} 
+
+	if(/(y+)/.test(format)) { 
+	format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+	} 
+
+	for(var k in o) { 
+	if(new RegExp("("+ k +")").test(format)) { 
+	format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length)); 
+	} 
+	} 
+	return format; 
+} 
+
 //校验邮箱是否合格
 function isEmail(email) {
 	var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
