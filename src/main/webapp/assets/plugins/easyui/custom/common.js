42e7668427826f3077ddbb22d6a6ba1511c687c4
Administrator <Administrator@MS-20160426JFHL>
2016-05-06 12:41:18 +0800

    发v

--- src/main/webapp/assets/plugins/easyui/custom/common.js
+++ src/main/webapp/assets/plugins/easyui/custom/common.js
@@ -78,6 +78,7 @@ function messagerCommon(){
 }
 
 /*文本框清除功能*/
+/* $('#tt').textbox().textbox('addClearBtn', 'icon-clear');*/
 $.extend($.fn.textbox.methods, {
     addClearBtn: function(jq, iconCls){
         return jq.each(function(){
@@ -106,6 +107,34 @@ $.extend($.fn.textbox.methods, {
         });
     }
 });
+
+//格式化日期框显示。 只显示年月， 参数 obj 必须是jquery对象
+function yearAndMonth(obj){
+	obj.datebox({
+        onShowPanel: function () {//显示日趋选择对象后再触发弹出月份层的事件，初始化时没有生成月份层
+            span.trigger('click'); //触发click事件弹出月份层
+            if (!tds) setTimeout(function () {//延时触发获取月份对象，因为上面的事件触发和对象生成有时间间隔
+                tds = p.find('div.calendar-menu-month-inner td');
+                tds.click(function (e) {
+                    e.stopPropagation(); //禁止冒泡执行easyui给月份绑定的事件
+                    var year = /\d{4}/.exec(span.html())[0]//得到年份
+                    , month = parseInt($(this).attr('abbr'), 10); //月份，这里不需要+1
+                    obj.datebox('hidePanel')//隐藏日期对象
+                    .datebox('setValue', year + '-' + month); //设置日期的值
+                });
+            }, 0)
+        },
+        parser: function (s) {
+            if (!s) return new Date();
+            var arr = s.split('-');
+            return new Date(parseInt(arr[0], 10), parseInt(arr[1], 10) - 1, 1);
+        },
+        formatter: function (d) { return d.getFullYear() + '-' + (d.getMonth()+1);/*getMonth返回的是0开始的，忘记了。。已修正*/ }
+    });
+    var p = obj.datebox('panel'), //日期选择对象
+    tds = false, //日期选择对象中月份
+    span = p.find('span.calendar-text'); //显示月份层的触发控件
+}
 
 /*转换树节点*/
 function convert(rows){
