1c321bedbe661d1d6d313b608e6744a18a14ed47
yt <676133829@qq.com>
2016-08-04 17:42:10 +0800

    template

---
+++ src/main/java/com/FangBianMian/websocket/WebSocketTest.java
@@ -0,0 +1,13 @@
+package com.FangBianMian.websocket;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketTest {
+
+}
