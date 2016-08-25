8ea1feec7972bf437e29f3348a2e1ec657cdb7ee
yt <676133829@qq.com>
2016-08-25 18:33:56 +0800

    template

--- src/main/java/com/FangBianMian/utils/DataUtil.java
+++ src/main/java/com/FangBianMian/utils/DataUtil.java
@@ -27,17 +27,71 @@ import javax.imageio.stream.MemoryCacheImageInputStream;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.poi.hssf.record.formula.functions.T;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 /**
  * @author Administrator
  * 
  */
 public class DataUtil {
+	
+	/**
+	 * 图片转化成base64字符串
+	 * @param path 图片路径
+	 * @return
+	 */
+	public static String base64GetImageStr(String path) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+		InputStream in = null;
+		byte[] data = null;
+		// 读取图片字节数组
+		try {
+			in = new FileInputStream(path);
+			data = new byte[in.available()];
+			in.read(data);
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// 对字节数组Base64编码
+		BASE64Encoder encoder = new BASE64Encoder();
+		return encoder.encode(data);// 返回Base64编码过的字节数组字符串
+	}
+
+	/**
+	 * base64字符串转化成图片
+	 * @param imgStr base64字符串
+	 * @param path 生成图片后的保存路径
+	 * @return
+	 */
+	public static boolean base64GenerateImage(String imgStr,String path) { // 对字节数组字符串进行Base64解码并生成图片
+		if (imgStr == null) // 图像数据为空
+			return false;
+		BASE64Decoder decoder = new BASE64Decoder();
+		try {
+			// Base64解码
+			byte[] b = decoder.decodeBuffer(imgStr);
+			for (int i = 0; i < b.length; ++i) {
+				if (b[i] < 0) {// 调整异常数据
+					b[i] += 256;
+				}
+			}
+			// 生成jpeg图片
+			String imgFilePath = path;// 新生成的图片
+			OutputStream out = new FileOutputStream(imgFilePath);
+			out.write(b);
+			out.flush();
+			out.close();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 	
 	/**
 	 * 判断集合是否为 null
