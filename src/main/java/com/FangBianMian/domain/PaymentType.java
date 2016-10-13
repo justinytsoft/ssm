78349ae90e71ba6edccc6c7c161d132ad5b221a7
yt <676133829@qq.com>
2016-10-13 13:59:14 +0800

    temp

---
+++ src/main/java/com/FangBianMian/domain/PaymentType.java
@@ -0,0 +1,25 @@
+package com.FangBianMian.domain;
+
+/**
+ * 支付方式类
+ * @author Administrator
+ *
+ */
+public class PaymentType {
+
+	private Integer id;
+	private String type;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
