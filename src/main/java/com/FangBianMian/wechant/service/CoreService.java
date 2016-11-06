d4e57430be72eef9a70e9c130fbb3f8d31e9fe40
justi <justi@192.168.0.101>
2016-11-06 23:32:02 +0800

    wechat

--- src/main/java/com/FangBianMian/wechant/service/CoreService.java
+++ src/main/java/com/FangBianMian/wechant/service/CoreService.java
@@ -21,7 +21,7 @@ public class CoreService {
         String respMessage = null;  
         try {  
             // 默认返回的文本消息内容  
-            String respContent = "请求处理异常，请稍候尝试！";  
+            String respContent = "对不起，亲，我还无法了解您的需求，我会不断改进的！";
   
             // xml请求解析  
             Map<String, String> requestMap = MessageUtil.parseXml(request);  
